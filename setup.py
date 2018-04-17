41c3ef64bb0e6371bb7afdffb3ed678069a95a84
Nathan Buckner <bucknath@amazon.com>
2018-04-17 10:24:59 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class Tox(TestCommand):
 
 setup(
     name='sshaolin',
-    version='0.0.5',
+    version='0.0.6',
     description='SSH for Ninjas',
     long_description='{0}'.format(open('README.md').read()),
     author='Nathan Buckner',
